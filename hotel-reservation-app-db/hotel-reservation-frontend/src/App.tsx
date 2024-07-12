bb63b75110d6abfd66eb706291ed07d82ff0b2be
Prem Prakash <iamprem021@gmail.com>
2024-07-12 18:02:19 +0530

    Implement authentication functionality for the web application 2

--- hotel-reservation-app-db/hotel-reservation-frontend/src/App.tsx
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/App.tsx
@@ -34,7 +34,6 @@ export default function App() {
     mobileNumber: userInfo?.mobile_number || "",
     };
     }
-    
     useEffect(() => {
     setIsAuthLoading(true);
     if (Cookies.get("userinfo")) {
--- hotel-reservation-app-db/hotel-reservation-frontend/src/layout/AppBar.tsx
+++ hotel-reservation-app-db/hotel-reservation-frontend/src/layout/AppBar.tsx
@@ -54,11 +54,12 @@ function UserMenu() {
             <Typography textAlign="center">My Reservations</Typography>
           </Button>
         </MenuItem>
-        <MenuItem onClick={() => {
-sessionStorage.removeItem("userInfo");
-window.location.href =
-`/auth/logout?session_hint=${Cookies.get('session_hint')}`;
-}}>
+        <MenuItem
+          onClick={() => {
+            sessionStorage.removeItem("userInfo");
+            window.location.href = `/auth/logout?session_hint=${Cookies.get("session_hint")}`;
+          }}
+        >
           <Button style={{ textTransform: "none" }}>
             <Typography textAlign="center">Logout</Typography>
           </Button>
